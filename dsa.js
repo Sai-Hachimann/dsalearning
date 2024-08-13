0e2c0a2fb1444916199cdc6e44a9335f5041ce28
hi <aikuros2496@gmail.com>
2024-08-13 14:18:18 +0530

    file added

---
+++ dsa.js
@@ -0,0 +1,59 @@
+// const nemo = ['nemo'];
+// const largeArray = new Array(10000).fill('nemo');
+
+// function findeNemo(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 'nemo') {
+//       console.log('Found Nemo');
+//     }
+//   }
+// }
+
+// findeNemo(largeArray);
+// const box = [0, 1, 2, 3, 4, 5];
+// function boxes(box) {
+//   console.log(box[0]);
+//   console.log(box[1]);
+// }
+
+// boxes(box);
+
+// const box1 = ['a', 'b', 'c', 'd', 'e'];
+
+// function arrayPair(input) {
+//   let pair = [];
+//   for (let i = 0; i < input.length; i++) {
+//     for (let j = 0; j < input.length; j++) {
+//       pair.push(input[i], input[j]);
+//     }
+//   }
+//   return pair;
+// }
+
+// console.log(arrayPair(box1));
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// function addPairs(numbers) {
+//   for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+//   }
+
+//   for (let q = 0; q < numbers.length; q++) {
+//     for (let j = 0; j < numbers.length; j++) {
+//       console.log(numbers[q] + numbers[j]);
+//     }
+//   }
+// }
+
+// addPairs(numbers);
+
+// function sum(n1, n2) {
+//   return n1[0] + n2[1];
+// }
+
+// console.log(sum(20, 10));
+
+let x = 5;
+let y = '5';
+console.log((x = y));
