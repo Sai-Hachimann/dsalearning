140c003828c8d49bcb89458d69fca19583004f30
sai <vsaisumanth241196@gmail.com>
2024-08-31 20:15:20 +0530

    frequency counter, char count

--- bigO.js
+++ bigO.js
@@ -23,3 +23,18 @@ function charCount(str) {
 console.log(charCount('hii how are you'));
 
 //frequency counters
+//naive approach
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indeOf(arr1[i] ** 2);
+    if (correctIndex === -1) {
+      return false;
+    }
+    arr2.splice(correctIndex, 1);
+  }
+  return true;
+}
